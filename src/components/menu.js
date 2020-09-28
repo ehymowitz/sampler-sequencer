b546c53d5640f31c8fa2153881a5bb2d846479bd
Elie Hymowitz <ehymowitz@gmail.com>
2020-09-28 13:57:33 -0400

    init

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-// import { FiMic } from 'react-icons/fi';
+import { FiMic } from 'react-icons/fi';
 import { BsFillPlayFill } from 'react-icons/bs';
 import { BiGridVertical } from 'react-icons/bi';
 import { Route, NavLink, HashRouter } from "react-router-dom";
-// import Mic from './mic';
+import Mic from './mic';
 import Mpc from './mpc';
 import Sequencer from './sequencer'
 import HoverTip from './tool-tip'
@@ -20,7 +20,7 @@ const Menu = () => {
         <div className = 'container'>
           <Route exact path ="/" component={ Sequencer }/>
           {
-            // <Route path ="/mic" component={ Mic } />
+            <Route path ="/mic" component={ Mic } />
           }
           <Route path ="/mpc" component={ Mpc } />
         </div>
@@ -30,12 +30,10 @@ const Menu = () => {
             <li style = {{ fontSize: "30px" }} >
               <NavLink to = "/" style={ content === 1 ? {color: "white" }: {}} onClick={()=>changeContent(1)}> <BsFillPlayFill/> </NavLink>
             </li>
-            {
-            // <div className="vertical-divider"></div>
-            // <li style = {{ fontSize: "20px" }} onClick={()=>changeContent(2)}>
-            //   <NavLink to = "/mic" style={ content === 2 ? {color: "white" }: {}}> <FiMic/> </NavLink>
-            // </li>
-            }
+            <div className="vertical-divider"></div>
+            <li style = {{ fontSize: "20px" }} onClick={()=>changeContent(2)}>
+              <NavLink to = "/mic" style={ content === 2 ? {color: "white" }: {}}> <FiMic/> </NavLink>
+            </li>
             <div className="vertical-divider"></div>
             <li style = {{ fontSize: "30px" }} >
               <NavLink to = "/mpc" style={ content === 3 ? {color: "white" }: {}} onClick={()=>changeContent(3)}> <BiGridVertical/> </NavLink>
--- src/components/mic.js
+++ src/components/mic.js
@@ -1,11 +1,32 @@
 import React from 'react'
+import Dropzone from 'react-dropzone'
 
 const Mic = () => {
+
+  const handleFiles = (files) => { // working here
+    const reader = new FileReader()
+    files.forEach(item => {
+      if (item.type === "audio/mpeg") {
+        console.log(item)
+        reader.readAsDataURL(item)
+      }
+      reader.addEventListener("loadend", (item.name) => {
+        sessionStorage.setItem(item.name, reader.result.toString())
+      })
+    })
+    console.log(sessionStorage)
+  }
+
   return (
-    <div>
-      <p>
-        TBD
-      </p>
+    <div className="mic-container">
+      <Dropzone onDrop= {acceptedFiles => handleFiles(acceptedFiles)}>
+        {({getRootProps, getInputProps}) => (
+          <div {...getRootProps()}>
+            <input {...getInputProps()} />
+            <p>Drag your audio files here! They'll be accessible on the other parts of the app</p>
+          </div>
+        )}
+      </Dropzone>
     </div>
   )
 }
