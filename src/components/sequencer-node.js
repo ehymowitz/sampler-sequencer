ef63a045c12f4796c7d22816a360f1d17fb9603c
Elie Hymowitz <ehymowitz@gmail.com>
2020-09-28 11:13:15 -0400

    fixed tone.start() bug

--- src/components/sequencer-node.js
+++ src/components/sequencer-node.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react'
+import * as Tone from 'tone'
 
 const SequencerNode = ({beat, note, octave, synth, spot, switchState, drum}) => {
 
   const [active, changeActive] = useState(false)
 
   const shouldPlay = () => {
+    Tone.start()
+
     if (active && spot === beat && switchState) {
       synth.value.triggerAttackRelease(`${note.value}${octave.value}`, "2n")
     }
